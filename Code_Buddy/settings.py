6e6d74b95ed3a5a34a954461a44117e9896c303d
utsav <putsavnarendra1998@gmail.com>
2020-08-10 12:05:11 +0530

    COMPRESS_ENABLED set to Fals

--- Code_Buddy/settings.py
+++ Code_Buddy/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICSTORAGE = "Whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 #     os.path.join(BASE_DIR, "staticfiles"),
